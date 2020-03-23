5e3554ec928e2ca891a0491549add04ebb40f353
ameijden <61517428+ameijden@users.noreply.github.com>
2020-03-23 17:34:58 +0100

    Final version

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,31 @@
 
 
-## set the value of the matrix
-## get the value of the matrix
-## calculate the inverse of the matrix
-## get the value of the inverse of the matrix
+## The makeCachematrix function 
+## First initaliaze objects x and m
+## x is initialized as a function argument, so no further initialization is required within the function
+## m is set to NULL, initializing it as an object within the makeCacheMatrix() environment to be used by later code in the function
+## x <<- y  # Assign the input argument to the x object in the parent environment
+## m <<- NULL # Clears any value of m that had been cached by a prior execution
+## get <- function() x # The symbol x is not defined within get(), R retrieves it from the parent environment of makeCacheMatrix().
+## setinverse <- function(solve) m <<- solve # To assign the input argument to the value of m in the parent environment
+## getinverse  <- function() m ## same as the symbol x, to find the correct symbol m to retrieve its value
+## list(set = set, # gives the name 'set' to the set() function defined above
+  ##   get = get,  # gives the name 'get' to the get() function defined above
+ ##    setinverse = setinverse, # gives the name 'setinverse' to the setinverse() function defined above
+  ##   getinverse = getinverse) # gives the name 'getinverse' to the getinverse() function defined above
+##list,  assigns each of these functions as an element within a list(), and returns it to the parent environment.
+
 
 
 makeCacheMatrix <- function(x = matrix()) {
 
   m <- NULL
   set <- function(y) {
-    x <<- y
+    x <<- y 
     m <<- NULL
   }
   get <- function() x
-  setinverse <- function(solve) m <<- mean
+  setinverse <- function(solve) m <<- solve
   getinverse <- function() m
   list(set = set, get = get,
        setinverse = setinverse,
@@ -23,13 +34,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-
-## Compute the inverse of the special matrix created above. 
-## It checks first to see if the inverse has already been calculated. 
-## If so, it gets the inverse from cache and skips the computation.
-## Otherwise it calculates the inverse of the matrix and sets the inverse in the cache
-
-
+## The cacheSolve function
+## cacheSolve() starts with a single argument, x, and an ellipsis that allows the caller to pass additional arguments into the function.
+## The function attempts to retrieve the inverse from the object passed in as the argument. First, it calls the getinverse() function on the input object.
+## then it checks to see whether the result is NULL. Since makecacheMatrix() sets the cached mean to NULL whenever a new vector is set into the object, 
+## if the value here is not equal to NULL, we have a valid, cached mean and can return it to the parent environment
+## if there is no cached data then the function will calculate the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
     m <- x$getinverse()
